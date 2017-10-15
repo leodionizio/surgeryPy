e64ca54f71336bfed3b08519ee19556bca1f2e47
Douglas Alves <douglas.expalves@gmail.com>
2017-10-15 14:05:27 -0200

    insert e selectAll funcionando

--- route.py
+++ route.py
@@ -2,7 +2,7 @@
 """ Json da response """
 import json
 
-from flask import Blueprint, make_response
+from flask import Blueprint, make_response, request
 
 from database import Mssql
 
@@ -10,25 +10,40 @@ __user__ = Blueprint('user', __name__)
 
 
 @__user__.route('/user', methods=['GET'])
-def create():
-    """ Criacao de usuario """
-    db = Mssql("localhost", 1433, "sa", 
-    "FocusAdmin7539", "SURGERY", True)
-    conexao = db.__conexao__()
-    payload = db.query(conexao, 'Select * from users')
-    reply = []
+def getallusers():
+    """ Get de usuarios """
+    _db = Mssql("localhost", 1433, "sa", "FocusAdmin7539", "SURGERY", False)
+    _db.__conexao__()
+    payload = _db.query('Select * from users')
+
+    reply = {"error": False, "code": 200}
+    data = list()
     for row in payload:
-        data = {
-            "data" : {
-                "id": row[0],
-                "nome": row[1],
-                "usuario": row[2],
-                "email": row[3],
-                "senha": row[4]
-            }
+        user = {
+            "id": row[0],
+            "nome": row[1],
+            "usuario": row[2],
+            "email": row[3],
+            "senha": row[4]
         }
-        reply.append(data)
-        
+        data.append(user)
+    reply["data"] = data
     reply = make_response(json.dumps(reply), 200)
 
     return reply
+
+
+@__user__.route('/user', methods=['POST'])
+def createuser():
+    """ Criacao de usuario """
+
+    _db = Mssql("localhost", 1433, "sa", "FocusAdmin7539", "SURGERY", True)
+    _db.__conexao__()
+    script = "INSERT INTO users (nome, usuario, email, password) VALUES(%s, %s, %s, %s) "
+    params = (request.form["nome"], request.form["usuario"],
+              request.form["email"], request.form["password"])
+    _db.querycommit(script, params)
+
+    response = {"message": "Sucesso"}
+
+    return make_response(json.dumps(response), 202)
